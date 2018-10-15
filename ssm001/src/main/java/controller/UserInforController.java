2b65e1c8d06a89305cd87c6816830065c26395fc
Your github name <13655187048@163.com>
2018-10-15 13:52:51 +0800

    提交

--- ssm001/src/main/java/controller/UserInforController.java
+++ ssm001/src/main/java/controller/UserInforController.java
@@ -1,7 +1,14 @@
 package controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/userinfor")
 public class UserInforController {
+    @GetMapping("/index")
+    public  String index(){
+        return  "123";
+    }
 }
